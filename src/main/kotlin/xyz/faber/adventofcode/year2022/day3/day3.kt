60b8204aab9bf2dadb735d6f19246ea4313bbc0c
Rasmus Faber-Espensen <rasmus@faber-espensen.dk>
2022-12-03 10:35:48 +0100

    Cleanup

--- src/main/kotlin/xyz/faber/adventofcode/year2022/day3/day3.kt
+++ src/main/kotlin/xyz/faber/adventofcode/year2022/day3/day3.kt
@@ -6,17 +6,18 @@ import xyz.faber.adventofcode.util.AdventSolution
 class Day3 : AdventSolution<Int>() {
     private fun priority(it: Char) = if (it in 'a'..'z') it - 'a' + 1 else it - 'A' + 27
     override fun part1(input: List<String>): Int {
-        return input.map { listOf(it.substring(0, it.length / 2), it.substring(it.length / 2)) }
-            .map { it.map { it.toCharArray().toSet() } }
+        return input.map { it }
+            .map { it.chunked(it.length / 2) }
+            .map { it.map { it.toSet() } }
             .map { (b1, b2) -> b1.intersect(b2).single() }
             .sumOf { priority(it) }
     }
 
     override fun part2(input: List<String>): Int {
-        return input.chunked(3)
-            .map { it.map { it.toCharArray().toSet() } }
-            .map { (b1, b2, b3) ->
-                b3.single { it in b1 && it in b2 }
+        return input.map { it }
+            .chunked(3)
+            .map { it.map { it.toSet() } }
+            .map { (b1, b2, b3) -> b1.intersect(b2).intersect(b3).single()
             }.sumOf { priority(it) }
     }
 }
