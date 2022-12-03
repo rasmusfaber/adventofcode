7903e2cf23a8af66402012c53489cbd111764ea3
Rasmus Faber-Espensen <rasmus@faber-espensen.dk>
2022-12-03 10:19:04 +0100

    Day 3

--- src/main/kotlin/xyz/faber/adventofcode/year2022/day3/day3.kt
+++ src/main/kotlin/xyz/faber/adventofcode/year2022/day3/day3.kt
@@ -4,12 +4,20 @@ import xyz.faber.adventofcode.util.AdventRunner
 import xyz.faber.adventofcode.util.AdventSolution
 
 class Day3 : AdventSolution<Int>() {
-    override fun part1(input: String): Int {
-        return 0
+    private fun priority(it: Char) = if (it in 'a'..'z') it - 'a' + 1 else it - 'A' + 27
+    override fun part1(input: List<String>): Int {
+        return input.map { listOf(it.substring(0, it.length / 2), it.substring(it.length / 2)) }
+            .map { it.map { it.toCharArray().toSet() } }
+            .map { (b1, b2) -> b1.intersect(b2).single() }
+            .sumOf { priority(it) }
     }
 
-    override fun part2(input: String): Int {
-        return 0
+    override fun part2(input: List<String>): Int {
+        return input.chunked(3)
+            .map { it.map { it.toCharArray().toSet() } }
+            .map { (b1, b2, b3) ->
+                b3.single { it in b1 && it in b2 }
+            }.sumOf { priority(it) }
     }
 }
 
--- src/main/kotlin/xyz/faber/adventofcode/year2022/dayN/dayN.kt
+++ src/main/kotlin/xyz/faber/adventofcode/year2022/dayN/dayN.kt
@@ -4,11 +4,11 @@ import xyz.faber.adventofcode.util.AdventRunner
 import xyz.faber.adventofcode.util.AdventSolution
 
 class DayN : AdventSolution<Int>() {
-    override fun part1(input: String): Int {
+    override fun part1(input: List<String>): Int {
         return 0
     }
 
-    override fun part2(input: String): Int {
+    override fun part2(input: List<String>): Int {
         return 0
     }
 }
