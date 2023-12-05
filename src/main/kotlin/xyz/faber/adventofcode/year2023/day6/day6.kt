185c2893073ca11b6d5bc855d1671ea79a91f4e2
Rasmus Faber-Espensen <r.faber+copilot@elsevier.com>
2023-12-05 10:08:01 +0100

    Prepare for day 6

---
+++ src/main/kotlin/xyz/faber/adventofcode/year2023/day6/day6.kt
@@ -0,0 +1,19 @@
+package xyz.faber.adventofcode.year2023.day6
+
+import xyz.faber.adventofcode.util.AdventRunner
+import xyz.faber.adventofcode.util.AdventSolution
+
+class Day6 : AdventSolution<Int>() {
+    override fun part1(input: List<String>): Int {
+        return 0
+    }
+
+    override fun part2(input: List<String>): Int {
+        throw NotImplementedError()
+    }
+}
+
+fun main(args: Array<String>) {
+    AdventRunner(2023, 6, Day6()).run()
+
+}
