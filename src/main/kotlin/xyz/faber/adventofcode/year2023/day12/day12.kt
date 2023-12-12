3c996555115ac65aa394052d65a658a7b6841f87
Rasmus Faber-Espensen <r.faber+copilot@elsevier.com>
2023-12-12 12:59:37 +0100

    Simplify day 12

--- src/main/kotlin/xyz/faber/adventofcode/year2023/day12/day12.kt
+++ src/main/kotlin/xyz/faber/adventofcode/year2023/day12/day12.kt
@@ -3,8 +3,14 @@ package xyz.faber.adventofcode.year2023.day12
 import arrow.core.Tuple5
 import xyz.faber.adventofcode.util.AdventRunner
 import xyz.faber.adventofcode.util.AdventSolution
+import xyz.faber.adventofcode.util.AdventSolutionWithTransform
 
-class Day12 : AdventSolution<Long>() {
+class Day12 : AdventSolutionWithTransform<Long, Pair<String, List<Int>>>() {
+
+  override fun transformLine(input: String): Pair<String, List<Int>> {
+    return input.split(' ')
+      .let { split -> split[0] to split[1].split(',').map { it.toInt() } }
+  }
 
   fun possibleArrangements(s: String, conditions: List<Int>) = possibleArrangements(s, 0, null, conditions, 0)
 
@@ -12,88 +18,77 @@ class Day12 : AdventSolution<Long>() {
 
   tailrec fun possibleArrangements(s: String, ps: Int, condition: Int?, conditions: List<Int>, pc: Int): Long {
     if (ps >= s.length) {
-      if ((condition == 0 || condition == null) && pc >= conditions.size) {
-        return 1
+      return if ((condition == 0 || condition == null) && pc >= conditions.size) {
+        1
       } else {
-        return 0
+        0
       }
     }
-    when (s[ps]) {
-      '.' -> {
-        if (condition == 0 || condition == null) {
-          return possibleArrangements(s, ps + 1, null, conditions, pc)
-        } else {
-          return 0
-        }
-      }
-
-      '#' -> {
-        if (condition == 0) {
-          return 0
-        }
-        if (condition == null) {
-          if (pc < conditions.size) {
-            return possibleArrangements(s, ps + 1, conditions[pc] - 1, conditions, pc + 1)
-          } else {
-            return 0
-          }
-        } else {
-          return possibleArrangements(s, ps + 1, condition - 1, conditions, pc)
-        }
-      }
-
-      '?' -> {
-        if (condition == null) {
-          if (pc < conditions.size) {
-            val cacheKey = Tuple5(s, ps, condition, conditions, pc)
-            val cached = cache[cacheKey]
-            if (cached != null) {
-              return cached
-            }
-            val res1 = possibleArrangements(s, ps + 1, null, conditions, pc)
-            val res2 = possibleArrangements(s, ps + 1, conditions[pc] - 1, conditions, pc + 1)
-            cache[cacheKey] = res1 + res2
-            return res1 + res2
-          } else {
-            return possibleArrangements(s, ps + 1, 0, conditions, pc)
-          }
-        } else {
-          if (condition > 0) {
-            return possibleArrangements(s, ps + 1, condition - 1, conditions, pc)
-          } else {
-            return possibleArrangements(s, ps + 1, null, conditions, pc)
-          }
-        }
-      }
-
+    return when (s[ps]) {
+      '.' -> handleWorking(condition, s, ps, conditions, pc)
+      '#' -> handleBroken(condition, s, ps, conditions, pc)
+      '?' -> handleUnknown(condition, s, ps, conditions, pc)
       else -> throw IllegalArgumentException("Bad character")
     }
   }
 
-  override fun part1(input: List<String>): Long {
-    return input.map {
-      it.split(' ')
-        .let { split -> split[0] to split[1].split(',').map { it.toInt() } }
+  private fun handleWorking(condition: Int?, s: String, ps: Int, conditions: List<Int>, pc: Int): Long {
+    return if (condition == 0 || condition == null) {
+      possibleArrangements(s, ps + 1, null, conditions, pc)
+    } else {
+      0
     }
-      .sumOf {
-        val r = possibleArrangements(it.first, it.second)
-        r
-      }
   }
 
-  override fun part2(input: List<String>): Long {
-    return input.map {
-      it.split(' ')
-        .let { split -> split[0] to split[1].split(',').map { it.toInt() } }
-    }
-      .sumOf {
-        val expandedString = (1..5).map { _ -> it.first }.joinToString("?")
-        val expandedConditions = (1..5).flatMap { _ -> it.second }
-        possibleArrangements(
-          expandedString,
-          expandedConditions
-        )
+  private fun handleBroken(condition: Int?, s: String, ps: Int, conditions: List<Int>, pc: Int): Long {
+    return if (condition == 0) {
+      0
+    } else if (condition == null) {
+      if (pc < conditions.size) {
+        possibleArrangements(s, ps + 1, conditions[pc] - 1, conditions, pc + 1)
+      } else {
+        0
       }
+    } else {
+      possibleArrangements(s, ps + 1, condition - 1, conditions, pc)
+    }
+  }
+
+  private fun handleUnknown(condition: Int?, s: String, ps: Int, conditions: List<Int>, pc: Int): Long {
+    if (condition == null) {
+      if (pc >= conditions.size) {
+        return possibleArrangements(s, ps + 1, 0, conditions, pc)
+      }
+      val cacheKey = Tuple5(s, ps, condition, conditions, pc)
+      return cache.getOrElse(cacheKey) {
+        val res1 = possibleArrangements(s, ps + 1, null, conditions, pc)
+        val res2 = possibleArrangements(s, ps + 1, conditions[pc] - 1, conditions, pc + 1)
+        cache[cacheKey] = res1 + res2
+        return res1 + res2
+      }
+    } else {
+      if (condition > 0) {
+        return possibleArrangements(s, ps + 1, condition - 1, conditions, pc)
+      } else {
+        return possibleArrangements(s, ps + 1, null, conditions, pc)
+      }
+    }
+  }
+
+
+  override fun part1(input: List<Pair<String, List<Int>>>): Long {
+    return input.sumOf { (string, conditions) -> possibleArrangements(string, conditions) }
+  }
+
+  override fun part2(input: List<Pair<String, List<Int>>>): Long {
+    return input.sumOf { (string, conditions) ->
+      val expandedString = (1..5).map { _ -> string }.joinToString("?")
+      val expandedConditions = (1..5).flatMap { _ -> conditions }
+      possibleArrangements(
+        expandedString,
+        expandedConditions
+      )
+    }
   }
 }
 
